b5d692c4c65175fd63eee21d7026b0df3f0114a4
sunny <kumar.sunny@tftus.com>
2020-10-16 17:23:52 +0530

    commit

--- test/test.js
+++ test/test.js
@@ -1,8 +1,7 @@
 
 const request = require('supertest');
-const app = require('../index');
 describe('GET /', () => {
     it('response with hello world', (done) => {
-        request(app).get('/').expect('hello world', done);
+
     })
 })
